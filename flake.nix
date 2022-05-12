8daf1b053618ae47c950b3596de5ba47ee5184cd
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:43 -0300

    chore: re index vla

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."vla-master".dir   = "master";
+  inputs."vla-master".owner = "nim-nix-pkgs";
+  inputs."vla-master".ref   = "master";
+  inputs."vla-master".repo  = "vla";
+  inputs."vla-master".type  = "github";
+  inputs."vla-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."vla-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
